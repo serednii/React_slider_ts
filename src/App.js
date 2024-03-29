8392c1e4c86e59dfa184b75310b16e035dfa6e5e
Serednii Mykola <seredniimykola@gmail.com>
2024-03-29 20:10:50 +0100

    deleted files redux-queri

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 
-import { addNewTodo, fetchTodos } from './store/todoSlice';
+import { addTodo } from './store/todoSlice';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 import ContainerCounter from './componentsCounter/ContainerCounter';
@@ -11,19 +11,15 @@ import { incCounter } from "./store/counterSlice";
 function App() {
   const [text, setText] = useState('');
   const dispatch = useDispatch();
-  const { status, error } = useSelector(state => state.todos)
 
   const handleAction = () => {
     if (text.trim().length) {
-      dispatch(addNewTodo(text));
+      dispatch(addTodo({ text }));
       dispatch(incCounter());
       setText('');
     }
   }
 
-  useEffect(() => {
-    dispatch(fetchTodos())
-  }, [dispatch])
 
   return (
     <div className='App'>
@@ -33,8 +29,6 @@ function App() {
         updateText={setText}
         handleAction={handleAction}
       />
-      {status === 'loading' && <h2>Loading...</h2>}
-      {error && <h2>An error occurred: {error}</h2>}
       <TodoList />
     </div>
   );
